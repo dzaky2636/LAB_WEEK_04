56e6985d9eef60a26e85e6729f2cf8efaeb69a30
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2024-09-17 11:30:01 +0700

    can finally run, but looks weird

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -1,38 +1,46 @@
 package com.example.lab_week_04
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_coffee -> {
+                loadFragment(ListFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_cafe -> {
+                loadFragment(CafeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-                    as NavHostFragment
-        val navController = navHostFragment.navController
-//Creating top level destinations
-//and adding them to the draw
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.listFragment, R.id.favoritesFragment, R.id.cafeFragment
-            ), findViewById(R.id.drawer_layout)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        findViewById<NavigationView>(R.id.nav_view)
-            ?.setupWithNavController(navController)
-// Added this part only
-        findViewById<BottomNavigationView>(R.id.bottom_nav)
-            ?.setupWithNavController(navController)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        // Load the default fragment
+        if (savedInstanceState == null) {
+            loadFragment(ListFragment())
+        }
     }
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) ||
-                super.onSupportNavigateUp()
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
