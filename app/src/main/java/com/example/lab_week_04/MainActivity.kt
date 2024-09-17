a5de81179f80de5c60e974080189746f18d1fade
Twilight <mario.richie.lim@gmail.com>
2024-09-17 11:57:58 +0700

    commented code

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.example.lab_week_04
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+//    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.navigation_coffee -> {
+//                loadFragment(ListFragment())
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_cafe -> {
+//                loadFragment(CafeFragment())
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+//        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+//
+//        // Load the default fragment
+//        if (savedInstanceState == null) {
+//            loadFragment(ListFragment())
+//        }
+//    }
+//
+//    private fun loadFragment(fragment: Fragment) {
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment_container, fragment)
+//            .commit()
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,7 +65,6 @@ class MainActivity : AppCompatActivity() {
         // Added this part only
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             ?.setupWithNavController(navController)
-
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
